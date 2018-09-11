72849669eaf24fad3769332c544b389b520b4f88
Jez Higgins <jez@jezuk.co.uk>
2018-09-11 14:09:59 +0100

    feat: Move rest-client state resource to its own plugin

--- test/express-spec.js
+++ test/express-spec.js
@@ -60,7 +60,8 @@ describe('Simple Express tests', function () {
             path.resolve(__dirname, './fixtures/plugins/endpoint-plugin'),
             require.resolve('@wmfs/tymly-rbac-plugin'),
             require.resolve('@wmfs/tymly-solr-plugin'),
-            require.resolve('@wmfs/tymly-users-plugin')
+            require.resolve('@wmfs/tymly-users-plugin'),
+            require.resolve('@wmfs/tymly-rest-client-plugin')
           ],
 
           blueprintPaths: [
