531f398a63684fa974ed585e6a93eb49f8e620f3
Jez Higgins <jez@jezuk.co.uk>
2020-04-20 15:03:41 +0100

    test: Removed unused stuff from fixture

--- test/fixtures/plugins/cats-plugin/components/state-resources/eating/index.js
+++ test/fixtures/plugins/cats-plugin/components/state-resources/eating/index.js
@@ -1,10 +1,6 @@
 'use strict'
 
 module.exports = class Eating {
-  boot (config, options, callback) {
-    callback(null)
-  }
-
   run (event, context) {
     console.log('EATING!!!!', context.executionName)
     event.petDiary.push(`Shh, ${event.petName} is eating...`)
--- test/fixtures/plugins/cats-plugin/components/state-resources/walking/index.js
+++ test/fixtures/plugins/cats-plugin/components/state-resources/walking/index.js
@@ -1,10 +1,6 @@
 'use strict'
 
 module.exports = class Walking {
-  boot (config, options, callback) {
-    callback(null)
-  }
-
   run (event, context) {
     console.log('WALKING....')
     event.petDiary.push(`${event.petName} is walking... where's ${event.gender === 'male' ? 'he' : 'she'} off to?`)
